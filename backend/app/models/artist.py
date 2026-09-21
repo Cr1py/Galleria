87f2c8e03e24ac56053462346ec519337446eceb
Christine Bautista <c.bautista2503@gmail.com>
2026-09-20 20:04:48 -0700

    made them in the wrong names :cries:

--- backend/app/models/artist.py
+++ backend/app/models/artist.py
@@ -1,28 +1,28 @@
 import uuid
-from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import base
 
 """
-export type Art = {
-  id: string;
-  artistId: string; // foreign key back to Artist's id
-  title: string;
-  imageUrl: string;
-  description?: string;
+export type Artist = {
+  id: string; 
+  email: string;
+  password: string;
+  name: string;
+  art: string; 
+  handle: string;  
 }
-
 """
 
 
-class Art(Base):
-    __tablename__ = "art"
+class Artists(Base):
+    __tablename__ = "artists"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    artistId = Column(UUID(as_uuid=True), ForeignKey("artists.id", ondelete="CASCADE"))
-    title = Column(String, nullable=False)
-    image_url = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    artist = relationship("Artist", back_populates="art")
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    handle = Column(String, unique=True, nullable=False)
+    art = relationship("Art", back_populates="artist", cascade="all, delete-orphan")
