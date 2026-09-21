a42a8bbe20bc6620c0ccf4c15492b34ed63dd54b
Christine Bautista <c.bautista2503@gmail.com>
2026-09-20 19:50:54 -0700

    created main

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="Galleria API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"],
+)
 
 
-@app.get("/health_check")
+@app.get("/")
 async def ping():
     return {"result": "healthy"}
